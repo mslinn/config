6033a515404982502bf9cf696eda9673d0017678
Mike Slinn <mslinn@micronauticsresearch.com>
2012-07-15 16:32:06 -0700

    wrote desperate plea for help after writting complete gibberish

--- src/main/scala/VersionPlugin.scala
+++ src/main/scala/VersionPlugin.scala
@@ -5,7 +5,30 @@ import java.net.URL
 import sbt._
 import scala.collection.mutable
 
+/* This is a mess. The SBT docs are incomprehensible.
+Here is what I want to do:
+1) Allow the user to specify a value for fetchFromUrl
+2) Use a default value of fetchFromUrl if not specified
+3) Use the value of _config to return the values of three lookups:
+   a) sbtDependencies (exposed as "V")
+   b) credentials (exposed as "creds")
+   c) servers
+Maybe some portion of the code below might be useful.
+*/
 object SbtDependencies extends Plugin {
+  // fixme how to set to sampleUrl if not user does not specify a value?
+  val fetchFromUrlKey = SettingKey[String]("fetch-from-url")
+  fetchFromUrlKey := fetchFromUrl
+
+  sbtDependencies <<= values
+
+  // this is probably not the way to expose the looked up values
+  object values {
+    val sbtDependencies = new Lookup("versions", "versions of dependencies")
+    val credentials     = new Lookup("credentials")
+    val servers         = new Lookup("servers", "servers")
+  }
+
   //val sampleUrl = new URL("https://raw.github.com/Bookish/config/master/src/scala/main/resource/definitions.conf")
   val sampleUrl = new URL("https://raw.github.com/Bookish/config/master/scalaBuild/Build.conf")
   val jarUrl    = new URL("file:///home/mslinn/.ivy2/local/com.bookish/config/scala_2.9.1/sbt_0.11.3/0.1.0-SNAPSHOT/jars/config.jar!/definitions.conf")
@@ -39,8 +62,4 @@ object SbtDependencies extends Plugin {
       value
     }
   }
-
-  lazy val V           = new Lookup("versions", "versions of dependencies")
-  lazy val credentials = new Lookup("credentials")
-  lazy val servers     = new Lookup("servers", "servers")
 }
