19e9f7cb28c029e8de916a7e8ef45102119bea16
Mike Slinn <mslinn@micronauticsresearch.com>
2012-07-16 11:30:09 -0700

    Clumsily lurching forward

--- src/main/scala/VersionPlugin.scala
+++ src/main/scala/VersionPlugin.scala
@@ -5,61 +5,65 @@ import java.net.URL
 import sbt._
 import scala.collection.mutable
 
-/* This is a mess. The SBT docs are incomprehensible.
-Here is what I want to do:
-1) Allow the user to specify a value for fetchFromUrl
-2) Use a default value of fetchFromUrl if not specified
-3) Use the value of _config to return the values of three lookups:
-   a) sbtDependencies (exposed as "V")
-   b) credentials (exposed as "creds")
-   c) servers
-Maybe some portion of the code below might be useful.
-*/
 object SbtDependencies extends Plugin {
-  // fixme how to set to sampleUrl if not user does not specify a value?
-  val fetchFromUrlKey = SettingKey[String]("fetch-from-url")
-  fetchFromUrlKey := fetchFromUrl
+  val fetchFromUrlKey = SettingKey[String]("fetch-from-url", "URL to fetch config file from")
 
-  sbtDependencies <<= values
+  val versionsTask = TaskKey[Lookup]("V", "Dependency version numbers parsed from config file")
+  versionsTask <<= fetchFromUrlKey map { (urlStr: String) => // never gets called
+    println("versionsTask was called")
+    fetchFromUrl = new URL(urlStr) // sets config as a side effect
+    new Lookup(config, "versions", "versions of dependencies") }
 
-  // this is probably not the way to expose the looked up values
-  object values {
-    val sbtDependencies = new Lookup("versions", "versions of dependencies")
-    val credentials     = new Lookup("credentials")
-    val servers         = new Lookup("servers", "servers")
-  }
+  val credentialsTask = TaskKey[Lookup]("credentials", "Userids and passwords parsed from config file")
+  credentialsTask <<= fetchFromUrlKey map { (urlStr: String) => // never gets called
+    println("credentialsTask was called")
+    fetchFromUrl = new URL(urlStr) // sets config as a side effect
+    new Lookup(config, "credentials") }
 
-  //val sampleUrl = new URL("https://raw.github.com/Bookish/config/master/src/scala/main/resource/definitions.conf")
-  val sampleUrl = new URL("https://raw.github.com/Bookish/config/master/scalaBuild/Build.conf")
+  val serversTask = TaskKey[Lookup]("servers", "Server URLs parsed from config file")
+  serversTask <<= fetchFromUrlKey map { (urlStr: String) => // never gets called
+    println("serversTask was called")
+    fetchFromUrl = new URL(urlStr) // sets config as a side effect
+    new Lookup(config, "servers", "servers") }
+
+  //val sampleUrl = new URL("https://raw.github.com/Bookish/config/master/src/scala/main/resource/definitions.conf") // uncomment after v2 branch is merged to master
+  val sampleUrl = new URL("https://raw.github.com/Bookish/config/master/scalaBuild/Build.conf") // delete after v2 branch is merged to master
   val jarUrl    = new URL("file:///home/mslinn/.ivy2/local/com.bookish/config/scala_2.9.1/sbt_0.11.3/0.1.0-SNAPSHOT/jars/config.jar!/definitions.conf")
 
-  private var _config: Config = ConfigFactory.empty
-  private def config: Config = _config
+  /** URL specified by user, defaults to value of `SbtDependencies.sampleUrl`.
+   * If called with the same URL twice, only fetches and parses URL the first time */
+  def fetchFromUrl_= (url: URL): Unit = {
+    if (_url == url)
+      return
 
-  private var _url: URL = sampleUrl
+    _url = url
+    _config = ConfigFactory.parseURL(_url)
+    println("SbtDependencies: fetched from %s:\n%s".format(url, _config.toString))
+  }
 
   /** URL specified by user, defaults to value of `SbtDependencies.sampleUrl` */
   def fetchFromUrl = _url
 
-  /** URL specified by user, defaults to value of `SbtDependencies.sampleUrl` */
-  def fetchFromUrl_= (url: URL): Unit = {
-    _url = url
-    _config = ConfigFactory.parseURL(url)
-  }
+  private var _config: Config = ConfigFactory.empty
+  private[config] def config: Config = _config
 
-  class Lookup(section: String, label: String="") {
-    val alreadyShown = mutable.HashSet.empty[String]
+  private var _url: URL = _url
+}
 
-    def apply(key: String) = {
-      val value: String = config.getString("definitions.%s.%s".format(section, key))
-      if (!alreadyShown.contains(key)) { // only display each key a maximum of one time
-          alreadyShown += key
-          if (label.length>0) // credential values are not displayed
-            println("  " + key + "=" + value)
-          else
-            println("  " + key + " was retrieved")
-      }
-      value
+class Lookup(config: Config, section: String, label: String="") {
+  val alreadyShown = mutable.HashSet.empty[String]
+  println("Created Lookup " + section)
+
+  def apply(key: String) = {
+    val value: String = config.getString("bookishDeps.%s.%s".format(section, key))
+    //val value: String = SbtDependencies.config.getString("definitions.%s.%s".format(section, key))
+    if (!alreadyShown.contains(key)) { // only display each key a maximum of one time
+        alreadyShown += key
+        if (label.length>0) // credential values are not displayed
+          println("  " + section + "." + key + "=" + value)
+        else
+          println("  " + section + "." + key + " was retrieved")
     }
+    value
   }
 }
