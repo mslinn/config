f2369fdc8b49375a48c478c72d797ed1103710d4
Mike Slinn <mslinn@micronauticsresearch.com>
2012-07-17 13:05:46 -0700

    Seems to work :)

--- src/main/scala/VersionPlugin.scala
+++ src/main/scala/VersionPlugin.scala
@@ -1,42 +1,45 @@
 package com.bookish.config
 
-import com.typesafe.config.{ Config, ConfigFactory }
+import com.typesafe.config.{ConfigValue, Config, ConfigFactory}
 import java.net.URL
 import sbt._
 import Keys._
 import scala.collection.mutable
+import collection.JavaConversions._
 
 object SbtDependencies extends Plugin {
   val configUrl            = SettingKey[String]("config-url", "URL to fetch config file from")
-  private val configConfig    = TaskKey[Config]("config-config")
-  val configVersionsLookup    = TaskKey[Lookup]("config-versions", "Dependency version numbers parsed from config file")
-  val configCredentialsLookup = TaskKey[Lookup]("config-credentials", "versions of dependencies")
-  val configServersLookup     = TaskKey[Lookup]("config-servers", "Userids and passwords parsed from config file")
 
   //val sampleUrl = "https://raw.github.com/Bookish/config/master/src/scala/main/resource/definitions.conf" // uncomment after v2 branch is merged to master
   val sampleUrl = "https://raw.github.com/Bookish/config/master/scalaBuild/Build.conf" // delete after v2 branch is merged to master
   val jarUrl    = "file:///home/mslinn/.ivy2/local/com.bookish/config/scala_2.9.1/sbt_0.11.3/0.1.0-SNAPSHOT/jars/config.jar!/definitions.conf"
 
+  val config = ConfigFactory.parseURL(new URL(sampleUrl)) // fixme only want to set to sampleUrl if not user does not specify a value.
+
   override def settings = Seq(
-    configUrl               := sampleUrl, // fixme only want to set to sampleUrl if not user does not specify a value.
-    configConfig            <<= configUrl    map { (urlStr: String) => ConfigFactory.parseURL(new URL(urlStr)) },
-    configVersionsLookup    <<= configConfig map (new Lookup(_, "versions", "versions of dependencies")),
-    configCredentialsLookup <<= configConfig map (new Lookup(_, "credentials")),
-    configServersLookup     <<= configConfig map (new Lookup(_, "servers", "servers"))/*,
-    libraryDependencies     <<= (configVersionsLookup, libraryDependencies) apply ( (values, deps) => {
-      // check deps for moduleIDs with group and artifact defined in values.sbtDependencies
-      // and set the configured version
-      // this needs a different layout of the config file
-      deps
-    }*/
-  )
+    configUrl := sampleUrl // fixme only want to set to sampleUrl if not user does not specify a value.
+  ) ++
+  makeSettings("V", new Lookup(config, "versions", "versions of dependencies")) ++
+  makeSettings("creds", new Lookup(config, "credentials")) ++
+  makeSettings("servers", new Lookup(config, "servers", "servers"))
 
-  case class Lookup(config: Config, section: String, label: String="") {
+  def makeSettings(prefix:String, lookup: Lookup): Seq[sbt.Project.Setting[_]] = {
+    (lookup.config.entrySet map { kv =>
+      val key = prefix + "." + kv.getKey
+      val value = kv.getValue.unwrapped.toString
+      val settingKey = SettingKey[String](key)
+      println("  %s = %s".format(key, value))
+      settingKey := value
+    }).toSeq
+  }
+
+  class Lookup(entireConfig: Config, val section: String, label: String="") {
     val alreadyShown = mutable.HashSet.empty[String]
-    println("Created Lookup " + section) // never called
+    println("Creating Lookup " + section)
+    val config = entireConfig.getConfig("bookishDeps.%s".format(section))
 
-    def read(key: String) = {
-      val value: String = config.getString("bookishDeps.%s.%s".format(section, key))
+    def apply(key: String) = {
+      val value: String = config.getString("%s".format(key))
       //val value: String = config.getString("definitions.%s.%s".format(section, key))
       if (!alreadyShown.contains(key)) { // only display each key a maximum of one time
           alreadyShown += key
